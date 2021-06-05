c85ef5b151179a87cad49bf3066fdcc15b9b14f2
Cristiano <contato@cristianoprogramador.com>
2021-06-05 09:30:44 -0300

    Cria client com C#

---
+++ GrpcClient-DotNet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+
+namespace GrpcClient_DotNet
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = new Channel("localhost:5000", ChannelCredentials.Insecure);
+            var client = new GrpcHost.Greeter.GreeterClient(channel);
+            var response = client.SayHello(new GrpcHost.HelloRequest() { Name = "Cristiano" });
+            Console.WriteLine(response.Message);
+            await channel.ShutdownAsync();
+        }
+    }
+}
